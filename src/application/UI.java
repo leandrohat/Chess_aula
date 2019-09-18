e17481263cc5ab753aa5fc4b8637927f5a3108ae
Leandro Hatimine <leandro.hatimine@gmail.com>
2019-09-18 18:55:28 -0300

    Check logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -58,6 +58,9 @@ public class UI {
 		System.out.println();
 		System.out.println("Turn "+chessMatch.getTurn());
 		System.out.println("waiting player "+ chessMatch.getCurrentPlayer());
+		if(chessMatch.getCheck()) {
+			System.out.println("Check!");
+		}
 	}
 	public static void printBoard(ChessPiece[][] pieces) {
 		for (int i = 0; i < pieces.length; i++) {
